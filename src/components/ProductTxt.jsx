ede7ba36462581ec7fe5c4ea4a697b19811377dd
Arefun Naher Nipa <arefunnnipa@gmail.com>
2024-11-12 21:25:42 +0600

    Update ProductTxt.jsx

--- src/components/ProductTxt.jsx
+++ src/components/ProductTxt.jsx
@@ -3,25 +3,40 @@ import Flex from './Flex'
 import Heading from './Heading'
 import { FaHeart, FaCodeCompare, FaCartShopping } from "react-icons/fa6";
 import Badge from './Badge';
+import Image from './Image';
 
-const ProductTxt = () => {
+const ProductTxt = ({badgeName,imgSrc,textOneH,textTwoH,textThreeH, pHeadingTxt,pPrice,pColor}) => {
     return (
         <>
-            <Badge badgeName={"new"}  className={"absolute top-5 left-5"}/>
-            <div className="absolute bottom-0 left-0 right-0 bg-white p-5 invisible group-hover:visible">
-                <Flex className={"justify-end gap-x-3 items-center"}>
-                    <Heading as={"h3"} className={"font-DMSans text-[16px] pb-2 text-menuColor hover:font-bold"} text={"Add to Wish List"} />
-                    <FaHeart />
-                </Flex>
-                <Flex className={"justify-end gap-x-3 items-center"}>
-                    <Heading as={"h3"} className={"font-DMSans text-[16px] pb-2 text-menuColor hover:font-bold"} text={"Compare "} />
-                    <FaCodeCompare />
-                </Flex>
-                <Flex className={"justify-end gap-x-3 items-center"}>
-                    <Heading as={"h3"} className={"font-DMSans text-[16px] text-menuColor hover:font-bold"} text={"Add to Cart"} />
-                    <FaCartShopping />
-                </Flex>
+            <div className="group">
+                <div className="relative">
+                    <Badge badgeName={badgeName} className={"absolute top-5 left-5"} />
+                    <Image imgSrc={imgSrc} imgAlt={"Image"} />
+
+                    <div className="absolute bottom-0 left-0 right-0 bg-white p-5 invisible group-hover:visible">
+                        <Flex className={"justify-end gap-x-3 items-center"}>
+                            <Heading as={"h3"} className={"font-DMSans text-[16px] pb-2 text-menuColor hover:font-bold"} text={textOneH} />
+                            <FaHeart />
+                        </Flex>
+                        <Flex className={"justify-end gap-x-3 items-center"}>
+                            <Heading as={"h3"} className={"font-DMSans text-[16px] pb-2 text-menuColor hover:font-bold"} text={textTwoH} />
+                            <FaCodeCompare />
+                        </Flex>
+                        <Flex className={"justify-end gap-x-3 items-center"}>
+                            <Heading as={"h3"} className={"font-DMSans text-[16px] text-menuColor hover:font-bold"} text={textThreeH} />
+                            <FaCartShopping />
+                        </Flex>
+                    </div>
+                </div>
+                <div className="py-3">
+                    <Flex className={"justify-between"}>
+                        <Heading as={"h2"} className={"font-DMSans font-bold text-[20px]"} text={pHeadingTxt} />
+                        <Heading as={"h4"} className={"font-DMSans text-menuColor text-[16px]"} text={pPrice} />
+                    </Flex>
+                    <Heading as={"h4"} className={"font-DMSans text-menuColor text-[16px] mt-3"} text={pColor} />
+                </div>
             </div>
+
         </>
     )
 }
